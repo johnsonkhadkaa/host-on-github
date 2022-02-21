f08ac6157db23c44273c51d30321ae08fd020915
JOHNSON KHADKA <er.johnsonkhadka@gmail.com>
2022-02-21 21:17:49 +0545

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 
 const express = require('express');
+const mongoose = require('mongoose');
 
+// const url = 'mongodb://localhost:27017/hostingDb'
 
 const apps= express();
+
+// mongoose.connect(url,{useNewUrlParser:true})
+// const con = mongoose.connection
+
 
 apps.get('/',(req,res)=>{
     res.send("Hello World!")
@@ -14,7 +20,7 @@ apps.get('/products', (req,res)=>{
 
 apps.get('/products/:id', (req,res)=>{
     const id = req.params.id;
-    res.send('products id: '+id)
+    res.send('<h2>product id:</h2>'+ id)
 })
 
 
